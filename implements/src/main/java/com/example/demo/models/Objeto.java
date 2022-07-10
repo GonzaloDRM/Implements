5c2835c45182382498b0ab1cac1248a6dd50e3f1
Gonzalo Del Rio <gonzalo_delrio@live.com.ar>
2022-07-10 02:37:25 -0300

    object

---
+++ implements/src/main/java/com/example/demo/models/Objeto.java
@@ -0,0 +1,33 @@
+package com.example.demo.models;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "OBJETO")
+@Data
+public class Objeto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Integer id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "NAME")
+    private Integer age;
+
+    @Column(name = "FRIENDS")
+    private List<String> friends;
+
+}
