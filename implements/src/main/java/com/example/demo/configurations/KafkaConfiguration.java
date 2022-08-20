2f3dabb2c52d7772dda4da290a470f8299f12888
Gonzalo Del Rio <gonzalo_delrio@live.com.ar>
2022-08-20 20:09:05 -0300

    add kafka methods

--- implements/src/main/java/com/example/demo/configurations/KafkaConfiguration.java
+++ implements/src/main/java/com/example/demo/configurations/KafkaConfiguration.java
@@ -3,34 +3,106 @@ package com.example.demo.configurations;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import com.example.demo.models.KafkaModel;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+import com.google.gson.Gson;
 
 @Configuration
 public class KafkaConfiguration {
 
     @Bean
-    ProducerFactory<String, KafkaModel> producerFactory(){
-      Map<String, Object> config = new HashMap<>();
-      
-      config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-      config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-      config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-      
-      return new DefaultKafkaProducerFactory<>(config);
+    ProducerFactory<String, KafkaModel> producerFactory() {
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    KafkaTemplate<String, KafkaModel> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    ConsumerFactory<String, KafkaModel> consumerFactory() {
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "myGroupId");
+
+        return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(),
+                new JsonDeserializer<>(KafkaModel.class));
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, KafkaModel> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, KafkaModel> concurrentKafkaListenerContainerFactory = new ConcurrentKafkaListenerContainerFactory<>();
+        concurrentKafkaListenerContainerFactory.setConsumerFactory(consumerFactory());
+
+        return concurrentKafkaListenerContainerFactory;
+    }
+
+    // Another way to do it with it's with Gson library
+    // -----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    @Bean
+    public Gson jsonConverter() {
+        return new Gson();
+    }
+
+    @Bean
+    ProducerFactory<String, String> producerFactoryWithGson() {
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+    
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    KafkaTemplate<String, String> kafkaTemplateWithGson() {
+        return new KafkaTemplate<>(producerFactoryWithGson());
+    }
+
+    @Bean
+    ConsumerFactory<String, String> consumerFactoryWithGson() {
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "myGroupId");
+
+        return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(),
+                new StringDeserializer());
     }
     
     @Bean
-    public KafkaTemplate<String, KafkaModel> kafkaTemplate(){
-        return new KafkaTemplate<>(producerFactory());
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactoryWithGson() {
+        ConcurrentKafkaListenerContainerFactory<String, String> concurrentKafkaListenerContainerFactory = new ConcurrentKafkaListenerContainerFactory<>();
+        concurrentKafkaListenerContainerFactory.setConsumerFactory(consumerFactoryWithGson());
+
+        return concurrentKafkaListenerContainerFactory;
     }
-    
 }
--- implements/src/main/java/com/example/demo/controllers/KafkaController.java
+++ implements/src/main/java/com/example/demo/controllers/KafkaController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,21 +9,45 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.models.KafkaModel;
+import com.google.gson.Gson;
 
 @RestController
 @RequestMapping("/api/kafka")
 public class KafkaController {
 
     private KafkaTemplate<String, KafkaModel> kafkaTemplate;
+    private KafkaTemplate<String, String> kafkaTemplateWithGson;
+    private Gson jsonConverter;
     
     @Autowired
-    public KafkaController(KafkaTemplate<String, KafkaModel> kafkaTemplate) {
+    public KafkaController(KafkaTemplate<String, KafkaModel> kafkaTemplate, KafkaTemplate<String, String> kafkaTemplateWithGson, Gson jsonConverter) {
         this.kafkaTemplate = kafkaTemplate;
+        this.kafkaTemplateWithGson = kafkaTemplateWithGson;
+        this.jsonConverter = jsonConverter;
     }
     
-    @PostMapping
+    @PostMapping 
     public void post(@RequestBody KafkaModel kafkaModel) {
         kafkaTemplate.send("Message", kafkaModel);        
+    }
+    
+    @KafkaListener(topics = "myTopic")
+    public void getFromKafka(KafkaModel kafkaModel) {
+        System.out.println(kafkaModel.toString());
+    }
+    
+    //With Gson library -----------------------------------------------------------------------------
+    
+    @PostMapping
+    public void postWithGson(@RequestBody KafkaModel kafkaModel) {
+        kafkaTemplateWithGson.send("Message", jsonConverter.toJson(kafkaModel));
+    }
+    
+    @KafkaListener(topics = "myTopic")
+    public void getFromKafkaWithGson(String kafkaModel) {
+        System.out.println(kafkaModel);
+        KafkaModel kafkaModel1 = (KafkaModel) jsonConverter.fromJson(kafkaModel, KafkaModel.class);
+        System.out.println(kafkaModel1.toString());
     }
     
 }
